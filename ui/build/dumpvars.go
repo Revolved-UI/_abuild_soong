9b86bb671e19896075f445c5d64d4de46facd4d4
SharmagRit <sharmagritstuff@gmail.com>
2024-01-27 07:19:07 +0100

    soong: Add Revolved ANSI art

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -176,17 +176,44 @@ var BannerVars = []string{
 }
 
 func Banner(make_vars map[string]string) string {
-	b := &bytes.Buffer{}
+        b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
-	for _, name := range BannerVars {
-		if make_vars[name] != "" {
-			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
-		}
-	}
-	fmt.Fprint(b, "============================================")
+                fmt.Fprintln(b, "==================================================================")
+                fmt.Fprintln(b, "██████╗ ███████╗██╗   ██╗ ██████╗ ██╗    ██╗   ██╗███████╗██████╗ ")
+                fmt.Fprintln(b, "██╔══██╗██╔════╝██║   ██║██╔═══██╗██║    ██║   ██║██╔════╝██╔══██╗")
+                fmt.Fprintln(b, "██████╔╝█████╗  ██║   ██║██║   ██║██║    ██║   ██║█████╗  ██║  ██║")
+                fmt.Fprintln(b, "██╔══██╗██╔══╝  ╚██╗ ██╔╝██║   ██║██║    ╚██╗ ██╔╝██╔══╝  ██║  ██║")
+                fmt.Fprintln(b, "██║  ██║███████╗ ╚████╔╝ ╚██████╔╝███████╗╚████╔╝ ███████╗██████╔╝")
+                fmt.Fprintln(b, "╚═╝  ╚═╝╚══════╝  ╚═══╝   ╚═════╝ ╚══════╝ ╚═══╝  ╚══════╝╚═════╝ ")
+                fmt.Fprintln(b, "                                                                  ")
+                fmt.Fprintln(b, "                           ██╗   ██╗██╗                           ")
+                fmt.Fprintln(b, "                           ██║   ██║██║                           ")
+                fmt.Fprintln(b, "                           ██║   ██║██║                           ")
+                fmt.Fprintln(b, "                           ██║   ██║██║                           ")
+                fmt.Fprintln(b, "                           ╚██████╔╝██║                           ")
+                fmt.Fprintln(b, "                            ╚═════╝ ╚═╝                           ")
+                fmt.Fprintln(b, "==================================================================")
+                fmt.Fprintln(b, "                     REVOLVED-UI by SharmagRit                    ")
+                fmt.Fprintln(b, "==================================================================")
+                fmt.Fprintf(b, "%s=%s\n", "REVOLVED_BUILD_INFO", make_vars["REVOLVED_BUILD_INFO"])
+                fmt.Fprintf(b, "%s=%s\n", "REVOLVED_BUILD_TYPE", make_vars["REVOLVED_BUILD_TYPE"])
+                fmt.Fprintf(b, "%s=%s\n", "REVOLVED_MAINTAINER", make_vars["REVOLVED_MAINTAINER"])
+                fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION", make_vars["PLATFORM_VERSION"])
+                fmt.Fprintf(b, "%s=%s\n", "PRODUCT_INCLUDE_TAGS", make_vars["PRODUCT_INCLUDE_TAGS"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_DEVICE", make_vars["TARGET_DEVICE"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_BUILD_VARIANT", make_vars["TARGET_BUILD_VARIANT"])
+                fmt.Fprintf(b, "%s=%s\n", "PRODUCT_INCLUDE_TAGS", make_vars["PRODUCT_INCLUDE_TAGS"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_ARCH", make_vars["TARGET_ARCH"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_ARCH_VARIANT", make_vars["TARGET_ARCH_VARIANT"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_TARGET_CPU_VARIANT", make_vars["TARGET_CPU_VARIANT"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_ARCH", make_vars["TARGET_2ND_ARCH"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_ARCH_VARIANT", make_vars["TARGET_2ND_ARCH_VARIANT"])
+                fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_CPU_VARIANT", make_vars["TARGET_2ND_CPU_VARIANT"])
+                fmt.Fprintf(b, "%s=%s\n", "BUILD_ID", make_vars["BUILD_ID"])
+                fmt.Fprintf(b, "%s=%s\n", "OUT_DIR", make_vars["OUT_DIR"])
+                fmt.Fprintln(b, "                                                          ")
 
-	return b.String()
+        return b.String()
 }
 
 func runMakeProductConfig(ctx Context, config Config) {
